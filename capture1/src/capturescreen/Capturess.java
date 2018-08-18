7013e05b0e3c3de72cd282673a9a141e8babf639
Bilal <Bilal@Bilalisha>
2018-08-18 16:27:53 +0530

    Bilal version1 08182018 4:27

---
+++ capture1/src/capturescreen/Capturess.java
@@ -0,0 +1,17 @@
+package capturescreen;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class Capturess {
+
+	@Test
+public void Capture()
+{
+WebDriver cap=new ChromeDriver();
+cap.get("https://www.facebook.com");
+
+	
+}
+}
